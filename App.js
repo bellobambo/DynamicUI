0b255df8ca66491352d52c1badac9266bbe4c147
bellobambo <bellobambo21@gmail.com>
2024-09-09 07:42:37 +0100

    dynamic ui

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, Platform, Pressable } from "react-native";
+import CustomButton from "./components/CustomButton/CustomButton";
+
+// import CustomButton from "./components/CustomButton/CustomButton.andriod";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome!</Text>
+        <CustomButton title="Press Me" onPress={() => alert("Pressed")} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "plum",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "plum",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+  box: {
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    ...Platform.select({
+      ios: {
+        color: "purple",
+        fontSize: 24,
+        fontStyle: "italic",
+      },
+      android: {
+        color: "blue",
+        fontSize: 24,
+        fontStyle: "italic",
+      },
+    }),
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
